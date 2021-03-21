3e47f9572602262c63e278774218533b2b14c23c
Blazej Szafranski <Szafranski@Blazejs-MacBook-Pro.local>
2021-03-21 18:06:35 +0100

    Updates

---
+++ precache-manifest.db944bbe5f006fe9e444f34ab1cecbed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e755b4ada36a140a93492cd599e1c048",
+    "url": "/cookbook/index.html"
+  },
+  {
+    "revision": "d61089a88878e2554b13",
+    "url": "/cookbook/static/css/main.a6d42177.chunk.css"
+  },
+  {
+    "revision": "a0d1e81ef23176b03838",
+    "url": "/cookbook/static/js/2.fa5a22bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cookbook/static/js/2.fa5a22bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d61089a88878e2554b13",
+    "url": "/cookbook/static/js/main.3b76fbf7.chunk.js"
+  },
+  {
+    "revision": "cd3e42fcdb8b5bb52844",
+    "url": "/cookbook/static/js/runtime-main.aa975e8c.js"
+  }
+]);
